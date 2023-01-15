f975899b4bf709802e68fa2052df3e04caf8759c
Tatiana Valanitskaya <tatiana010fi@gmail.com>
2023-01-15 18:09:45 +0300

    First commit

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,4 +1,3 @@
-import { async } from "q";
 import { usersAPI } from "../api/api";
 
 const FOLLOWED = 'FOLLOW';
@@ -11,11 +10,6 @@ const TOGGLE_IS_FOLLOWING_PROGRESS ='TOGGLE_IS_FOLLOWING_PROGRESS';
 
 let initialState = {
     users:[
-    //     { id: 1, followed: true, fullName: "Dmitriy", avatar:"https://images.unsplash.com/photo-1478098711619-5ab0b478d6e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80", status: 'Money is power', location:{city: "Sochi", country:"Russia",}},
-    //     { id: 2, followed: false, name: "Andrey", avatar:"https://images.unsplash.com/photo-1497993950456-cdb57afd1cf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80", status: 'Knowledge is power', location:{city: "Moscow", country:"Russia",}},
-    //     { id: 3, followed: false, name: "Elena",avatar:"https://images.unsplash.com/photo-1568822617270-2c1579f8dfe2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",status: 'On work', location:{city: "Krasnodar", country:"Russia",}},
-    //     { id: 4,followed: false, name: "Kate", avatar:"https://images.unsplash.com/photo-1581467655410-0c2bf55d9d6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80",status: 'Trip', location:{city: "Minsk", country:"Belarus",}},
-    //     { id: 5,followed: false, name: "Sergey", avatar:"https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=932&q=80",status: 'Boss is here', location:{city: "Sochi", country:"Russia",} },
       ],
     pageSize: 5,
     totalUsersCount:0,
